bb25d630f6020a78b1bab0b78c97a538d0db9d21
IsaiahSama <zeldaplayergl@gmail.com>
2024-09-07 19:04:22 -0400

    Added html for hangman game

---
+++ words.js
@@ -0,0 +1,14 @@
+// This file will contain a variable for the words that hangman will use
+
+words = [
+    "hello",
+    "goodbye",
+    "computer",
+    "keyboard",
+    "mouse",
+    "python",
+    "javascript",
+    "html",
+    "css",
+    // Add more words here
+]
